4ad8ddaff801d915463f737d9360dc170d805f3e
Benjamin <benjamin.garcia@aluno.senai.br>
2026-08-13 10:56:56 -0300

    Alterações aula 13/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -4,6 +4,8 @@
  */
 package todolist;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aluno
@@ -14,7 +16,25 @@ public class ToDoList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner (System.in);
+        
+        String[] tarefas = new String[10];
+        boolean[] concluidas = new boolean[10];
+        
+        int opcao = 0;
+        
+        while(opcao != 5){
+            System.out.println("=== LISTA DE TAREFAS ===");
+            System.out.println("1- Adicionar tarefa");
+            System.out.println("2- Listar tarefas");
+            System.out.println("3- Concluir tarefas");
+            System.out.println("4- Excluir tarefa");
+            System.out.println("5- Sair");
+            
+            System.out.println("Digite a opcao que voce quer: ");
+            int escolha = scanner.nextInt();
+        }
+
     }
     
 }
