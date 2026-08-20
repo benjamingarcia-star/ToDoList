2a44ee82b9cf5a91290f15e8594c0f79de09785f
Benjamin <benjamin.garcia@aluno.senai.br>
2026-08-20 09:05:11 -0300

    Alteração aula dia 20/08

--- src/ToDoListProfessor.java
+++ src/ToDoListProfessor.java
@@ -85,6 +85,26 @@ public class ToDoListProfessor {
                     }else{
                         System.out.println("Tarefa Inválida!");
                     }
+                case 4:
+                    System.out.println("Digite o numero da tarefa que deseja excluir");
+                    int numExcluir = scanner.nextInt();
+                    
+                    int indiceExcluir = numExcluir - 1;
+                    
+                    if (indiceExcluir >= 0 && indiceExcluir < tarefas.length && tarefas[indiceExcluir] != null){
+                        tarefas[indiceExcluir] = null;
+                        concluidas[indiceExcluir] = false;
+                        
+                        System.out.println("Tarefa excluida com sucesso");
+                    }else{
+                        System.out.println("Tarefa invalida");
+                    }
+                    break;
+                    
+                case 5:
+                    System.out.println("Programa encerrado, muito obrigado!");
+                    
+                    break;
         }
     }
 }
